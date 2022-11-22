23ad6a00a9e3dc8f1e66d996f531c02b3fb0a3af
Aleksi1212 <aleksinoro@gmail.com>
2022-11-22 16:40:24 +0200

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import home from './images/home.png'
+
+
+function Navbar() {
+    return (
+        <>
+            <div className="flex justify-between h-28 fixed w-screen">
+                <div className="flex justify-center items-center w-48 mt-14 ml-10">
+                    <img className="shadow" src={home} alt="home" width={35} height={35}/>
+                </div>
+
+                <div className="flex justify-evenly w-2/5 mr-10">
+                    <div className="svg-wrapper">
+                        <svg height={60} width={200} xmlns="http://www.w3.org/2000/svg">
+                            <rect className="animation" height={60} width={100} rx={30} x={2}/>
+                        </svg>
+                        <button className="navbar-button">
+                            <a href="#about">About</a>
+                        </button>
+                    </div>
+                    
+                    <div className="svg-wrapper">
+                        <svg height={60} width={200} xmlns="http://www.w3.org/2000/svg">
+                            <rect className="animation" height={60} width={100} rx={30} x={2}/>
+                        </svg>
+                        <button className="navbar-button">
+                            <a href="#projects">Projects</a>
+                        </button>
+                    </div>
+
+                    <div className="svg-wrapper">
+                        <svg height={60} width={200} xmlns="http://www.w3.org/2000/svg">
+                            <rect className="animation" height={60} width={100} rx={30} x={2}/>
+                        </svg>
+                        <button className="navbar-button">
+                            <a href="#contact">Contact</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
