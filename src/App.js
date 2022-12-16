88f3b6573aca3199faf6fbe35afa82f2aa915c86
Aleksi1212 <aleksinoro@gmail.com>
2022-12-16 10:58:01 +0200

    favicon cahnges

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Navbar from "./components/Navbar";
 import Contact from "./components/Contactform";
-import icon from './images/icon.png'
 
 function App() {
   return (
     <>
-      <link rel="icon" href={icon} />
       <Navbar />
       <Contact />
     </>
